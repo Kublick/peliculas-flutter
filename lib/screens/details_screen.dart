a904321d34645f9c44e54479c083838e889cdba6
Max Blancarte <blancarte@gmail.com>
2022-08-07 01:40:44 -0700

    feat: poster and title

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -7,7 +7,13 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final String movie =
         ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-null';
-    return const Scaffold(body: CustomScrollView(slivers: [_CustomAppBar()]));
+    return Scaffold(
+        body: CustomScrollView(slivers: [
+      const _CustomAppBar(),
+      SliverList(
+        delegate: SliverChildListDelegate([const _PosterAndTitle()]),
+      )
+    ]));
   }
 }
 
@@ -37,5 +43,55 @@ class _CustomAppBar extends StatelessWidget {
               placeholder: AssetImage('assets/loading.gif'),
               fit: BoxFit.cover)),
     );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: const FadeInImage(
+                  image: NetworkImage('https://picsum.photos/200/300'),
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  height: 150,
+                )),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'movie-title',
+                  style: textTheme.headline5,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                Text(
+                  'movie.originalTitle',
+                  style: textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.star_outline,
+                        size: 15, color: Colors.grey),
+                    const SizedBox(width: 5),
+                    Text('movie.voteAverage', style: textTheme.caption)
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
